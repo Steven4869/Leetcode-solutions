f002084fb3000d40b62f9eb709475432ac6d5a6c
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-01-06 11:56:45 +0530

    Time: 4 ms (76.38%), Space: 13.7 MB (6.62%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,44 +11,21 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        //Iterative using BFS
-        //Intialise queue 
-        queue<TreeNode *>q;
-        //Intialise the result vector to store the final result
-        vector<vector<int>>result;
-        //Base condition 
-        if(root==NULL)
-            return result;
-        //Insert root node into queue 
-        q.push(root);
-        //Run the loop until queue is empty 
-        while(!q.empty()){
-            // Check the size of the queue 
-            int n = q.size();
-            //Make the answer vector to store the level elements into that vector 
-            vector<int>ans;
-            //Run the loop till the size 
-            for(int i=0;i<n;i++){
-                //Assign the temp value 
-                TreeNode *temp = q.front();
-                //Pop the value from the queue 
-                q.pop();
-                
-                //Insert the answer into queue
-                ans.push_back(temp->val);
-                
-                //Check for the left and right 
-                //Check if the left or right of temp exists or not 
-                //If exists then insert into queue 
-                if(temp->left)
-                    q.push(temp->left);
-                if(temp->right)
-                    q.push(temp->right);
-            }
-            //Store the answer into result 
-            result.push_back(ans);
+    vector<vector<int>>result;
+    void dfs(TreeNode *root, int level){
+        if(!root)
+            return;
+        if(result.size()==level){
+            result.push_back({});
         }
+        result[level].push_back(root->val);
+        dfs(root->left, level+1);
+        dfs(root->right, level+1);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        //Recursive approach using DFS
+        dfs(root, 0);
         return result;
+        
     }
 };
