7cc6642cee3e17f58d45b8c85b1c20e4215c4c7d
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-02-03 10:29:21 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (97.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
+    int dp[101][101];
+    int recur(int m, int n, int i, int j){
+        if(i>=m||j>=n)
+            return 0;
+        if(i==m-1&&j==n-1)
+            return 1;
+        if(dp[i][j])
+            return dp[i][j];
+        return dp[i][j] = recur(m,n,i+1,j)+recur(m,n,i,j+1);
+    }
     int uniquePaths(int m, int n) {
-        //Math Solution 
-        //n+m-1 C n-1
-        
-        int N = n+m-2;
-        int r = m-1;
-        double result =1;
-        
-        for(int i=1;i<=r;i++){
-            result = result*(N-r+i)/i;
-        }
-        return result;
-        
+        return recur(m, n, 0, 0);
     }
 };
