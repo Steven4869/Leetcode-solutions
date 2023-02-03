c520916bf798b2d73fe961948bf0b5dfd61e2f2a
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-02-03 11:13:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.94%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int dp[46];
+    int climbStairs(int n) {
+        if(n<=1)
+            return 1;
+        if(dp[n])
+            return dp[n];
+        return dp[n] = climbStairs(n-1)+climbStairs(n-2);
+    }
+};
