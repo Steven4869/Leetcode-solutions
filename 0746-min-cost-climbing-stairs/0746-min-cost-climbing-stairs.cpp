be75d2bb4ac4453a302b55aa7953d674f1199cb1
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-05-22 20:05:26 +0530

    Time: 4 ms (76.43%), Space: 13.8 MB (27.36%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size();
-        vector<int>dp(n+1, -1);
-        return min(minCost(cost, n-1, dp), minCost(cost, n-2, dp));
-    }
-    int minCost(vector<int>& cost, int n, vector<int>&dp){
-        if(n<0)
-            return 0;
-        if(n==0||n==1)
-            return cost[n];
-        if(dp[n]!=-1)
+    int solve(vector<int>&cost, int n, vector<int>&dp){
+        //Base Case 
+        if(n==0)
+            return cost[0];
+        if(n==1)
+            return cost[1];
+        if(dp[n]!=-1){
             return dp[n];
-        dp[n] = cost[n]+min(minCost(cost, n-1, dp), minCost(cost, n-2, dp));
-        return dp[n];
-        //Above approach would led to TLE, so we'll have to use the memoisation 
+        }
+        return dp[n] = cost[n]+min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n =cost.size();
+        vector<int>dp(n+1, -1);
+        int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        return ans;
     }
 };
