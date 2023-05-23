bea02b7c8f0eb32725ebc2d3bd7a7dbafedae852
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-05-23 10:06:08 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (28.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int recur(vector<int>& nums, int idx, vector<int>&dp){
+    //Based on the operation, that if we include then raise the index by 2 otherwise if we are 
+    //excluding then raise the index by 1
+    
+    int solve(vector<int>&nums, int idx, vector<int>&dp){
+        //Base Case 
         if(idx>=nums.size())
             return 0;
         if(dp[idx]!=-1)
             return dp[idx];
-        return dp[idx]=max(nums[idx]+recur(nums, idx+2, dp), recur(nums, idx+1, dp));
-        //It takes O(2^N) time
-        //we have to reduce the time with dp memo 
+        int include = nums[idx]+solve(nums, idx+2, dp);
+        int exclude = 0 + solve(nums, idx+1, dp);
+        int ans = max(include, exclude);
+        return dp[idx] = ans;
     }
     int rob(vector<int>& nums) {
-        //This is a simple 1-D DP Problem 
-        //Find the recurrence relation 
-        //Robber can do two jobs: he'll rob the house or will not rob the house ]
-        //If he chooses the first house the amount will be money from the (i-2)th house+
-        //money from the ith house 
-        //Otherwise rob from the money of (i-1)th house 
         vector<int>dp(nums.size()+1, -1);
-        return recur(nums, 0, dp);
+        return solve(nums, 0, dp);
     }
 };
