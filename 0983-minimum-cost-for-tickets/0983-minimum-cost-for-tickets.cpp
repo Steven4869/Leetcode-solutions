da5784c5430caba8ae79dc4974bf4972af3e4ce3
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-05-25 13:48:16 +0530

    Time: 9 ms (19.11%), Space: 9.9 MB (22.09%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -23,8 +23,31 @@ public:
         
         return dp[idx]=min(option1, min(option2, option3));
     }
+    
+    //Space Optimisation 
+    
+    int solve2(vector<int>&days, vector<int>&costs){
+        queue<pair<int,int>>weekly;
+        queue<pair<int,int>>monthly;
+        int ans = 0;
+        for(int day:days){
+            //Remove expired days
+            while(!weekly.empty()&&weekly.front().first+7<=day)
+                weekly.pop();
+            while(!monthly.empty()&&monthly.front().first+30<=day)
+                monthly.pop();
+            
+            //Add cost for current days 
+            weekly.push(make_pair(day, ans + costs[1]));
+            monthly.push(make_pair(day, ans + costs[2]));
+            
+            ans = min(ans+costs[0], min(weekly.front().second, monthly.front().second));
+        }
+        return ans;
+    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         vector<int>dp(days.size()+1, -1);
-        return solve(days, costs, 0, dp);
+        // return solve(days, costs, 0, dp);
+        return solve2(days, costs);
     }
 };
