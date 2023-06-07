a3d287d88ba13716723df2fe5161df569a82f0d3
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-06-07 17:29:07 +0530

    Time: 7 ms (72.70%), Space: 15.5 MB (17.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,15 +1,56 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        //We'll only get the profit line for the answer 
-        //That is obtaining the increasing line 
-        //We'll start from 1st index and go to the size of array
-        //check the previous index if it's smaller than the later or not
-        int profit = 0;
-        for(int i=1;i<prices.size();i++){
-            if(prices[i-1]<prices[i])
-                profit=profit + (prices[i]-prices[i-1]);
+    int solve(vector<int>&prices, int buy, int index){
+        //Base Case 
+        if(index == prices.size())
+            return 0;
+        int profit =0;
+        
+        //If the buying of stock is allowed 
+        if(buy){
+            //Stock Bought, so representing it as negative, stock not bought so skipping it
+            //Take the maximum out of those 
+            profit = max((-prices[index])+solve(prices,0,index+1), 0 + solve(prices, 1, index+1));
+        }
+        else{
+            //If buying is not true then we can sell
+            //Now we can sell it or skip to the next index 
+            profit = max((prices[index])+solve(prices,1,index+1), 0 + solve(prices, 0, index+1));
         }
         return profit;
+    }
+    int solveMemo(vector<int>&prices, int buy, int index, vector<vector<int>>&dp){
+        //Base Case 
+        if(index == prices.size())
+            return 0;
+        int profit =0;
+        
+        if(dp[buy][index]!=-1){
+            return dp[buy][index];
+        }
+        
+        //If the buying of stock is allowed 
+        if(buy){
+            //Stock Bought, so representing it as negative, stock not bought so skipping it
+            //Take the maximum out of those 
+            profit = max((-prices[index])+solveMemo(prices,0,index+1, dp), 0 + solveMemo(prices, 1, index+1, dp));
+        }
+        else{
+            //If buying is not true then we can sell
+            //Now we can sell it or skip to the next index 
+            profit = max((prices[index])+solveMemo(prices,1,index+1, dp), 0 + solveMemo(prices, 0, index+1, dp));
+        }
+        return dp[buy][index]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        //We have three ways, one to buy, one to skip and one to sell it
+        //We will be setting up buy=1 => allowed to buy the stock 
+        //If the buy = 0 => Not allowed to buy 
+        
+        // return solve(prices, 1, 0);
+        
+        //Using Memoisation 
+        vector<vector<int>>dp(2, vector<int>(prices.size()+1, -1));
+        return solveMemo(prices, 1, 0, dp);
     }
 };
