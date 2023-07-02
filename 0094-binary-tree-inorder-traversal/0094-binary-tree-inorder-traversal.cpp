c75cc6b693121cd0384a9218815be254975a4127
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-02 15:21:36 +0530

    Time: 6 ms (23.25%), Space: 8.3 MB (56.53%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -19,10 +19,27 @@ public:
         result.push_back(root->val);
         solve(root->right, result);
     }
+    void solveStack(TreeNode *root, vector<int>&result){
+        stack<TreeNode*>st;
+        TreeNode *curr = root;
+        while(curr!=NULL || !st.empty()){
+            while(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }
+            curr=st.top();
+            result.push_back(curr->val);
+            st.pop();
+            if(curr!=NULL){
+                curr=curr->right;
+            }
+        }
+    }
     vector<int> inorderTraversal(TreeNode* root) {
         
         vector<int>result;
-        solve(root, result);
+        // solve(root, result);
+        solveStack(root, result);
         return result;
     }
 };
