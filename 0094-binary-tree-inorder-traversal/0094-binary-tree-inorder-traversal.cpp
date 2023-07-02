0b425733951db0b7b5350b91ac910a0df71ec450
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-02 15:29:38 +0530

    Time: 6 ms (23.25%), Space: 8.3 MB (88.09%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -35,11 +35,43 @@ public:
             }
         }
     }
+    void morrisTraversal(TreeNode *root, vector<int>&result){
+        //Make the current node point to the root
+        TreeNode *curr = root;
+        //Check if the left subtree is null or not 
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                //Then print the data and point to the right 
+                result.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                //Find the rightmost node of the left node 
+                TreeNode *pre = curr->left;
+                //Traverse until we find the rightmost node and it                  is not equal to the current 
+                while(pre->right!=NULL && pre->right!=curr){
+                    pre=pre->right;
+                }
+                //If the right node is NULL then make up the temp                      link
+                if(pre->right==NULL){
+                    pre->right = curr;
+                    curr=curr->left;
+                }
+                //Otherwise remove the link and move to the right
+                else{
+                    pre->right=NULL;
+                    result.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+    }
     vector<int> inorderTraversal(TreeNode* root) {
         
         vector<int>result;
         // solve(root, result);
-        solveStack(root, result);
+        // solveStack(root, result);
+        morrisTraversal(root, result);
         return result;
     }
 };
