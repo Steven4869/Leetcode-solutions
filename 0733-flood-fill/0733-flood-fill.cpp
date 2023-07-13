a86b6a23c218dfdbfb5e88b8b7c12932218a59cb
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-13 23:25:44 +0530

    Time: 7 ms (88.35%), Space: 13.8 MB (87.16%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     
-    void dfs(vector<vector<int>>& image, int sr, int sc, int rows, int cols, int color, int source){
-        //Base Boundary Conditions
-        if(sr<0||sr>=rows||sc<0||sc>=cols)
+    void dfs(vector<vector<int>>&image, int sr, int sc, int rows, int cols, int color, int source){
+        //Boundary Conditions 
+        if(sr<0||sr>=rows||sc<0||sc>=cols){
             return;
-        else if(image[sr][sc]!=source)
-            return;
-        //Make the selected node to the color
-        image[sr][sc]=color;
-        //Calling 4 directional DFS Calls 
-        dfs(image, sr-1, sc, rows, cols, color, source);//Top
-        dfs(image, sr+1, sc, rows, cols, color, source);//Bottom 
-        dfs(image, sr, sc-1, rows, cols, color, source);//Left
-        dfs(image, sr, sc+1, rows, cols, color, source);//Right
-        
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        //The idea is that we have to get the source node from given sr,sc values
-        //Then change the value of node to the color 
-        //Then recurively call the all 4 directions and check if the node is equal to source nodes
-        //If they are equal then change it to color 
-        //Basic DFS Traversal is followed 
-        //TC:O(MXN) SC:(1)
-        
-        //Get rows and cols of the image 
-        int rows = image.size();
-        int cols = image[0].size();
-        //Get the source node 
-        int source = image[sr][sc];
-        //If the source node is not equal to the color then call the recursive function 
-        if(source!=color){
-            dfs(image, sr, sc, rows, cols, color, source);
         }
-        //Returning the image grid after making changes 
+        else if(source!=image[sr][sc]){
+            return;
+        }
+        image[sr][sc] = color;
+        
+        //Then Call DFS in 4 directions 
+        dfs(image, sr-1, sc, rows, cols, color, source);
+        dfs(image, sr+1, sc, rows, cols, color, source);
+        dfs(image, sr, sc-1, rows, cols, color, source);
+        dfs(image, sr, sc+1, rows, cols, color, source);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)    {
+       //Get the rows and columns 
+        int rows = image.size();
+        int columns = image[0].size();
+        
+        //Get the source 
+        int source = image[sr][sc];
+        
+        if(source!=color){
+            dfs(image, sr, sc, rows, columns, color, source);
+        }
         return image;
     }
 };
