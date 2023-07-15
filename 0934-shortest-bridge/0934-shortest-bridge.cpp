d258b17e69fd5b905db280b8e799c3f86183876f
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-15 16:23:32 +0530

    Time: 39 ms (99.10%), Space: 20.2 MB (64.46%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,73 +1,66 @@
 class Solution {
 public:
-    
-    queue<pair<int, int>> q; // queue
-    
-    void dfs(vector<vector<int>>& grid, int i, int j) { // for marking the 1st island visited
-        
-        if (i<0 or i>=grid.size() or j<0 or j>=grid[0].size() or grid[i][j] == 2 or grid[i][j] == 0)
+    void dfs(vector<vector<int>>&grid, int i, int j, queue<pair<int,int>>&q){
+        //Base conditions 
+        int rows = grid.size();
+        int cols = grid[0].size();
+        if(i<0 || i>=rows || j<0 || j>=cols || grid[i][j] !=1)
             return;
-        
-        grid[i][j] = 2; // marking them visited
-        q.push({i,j}); // adding them to queue
-        
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
+        grid[i][j]=2;
+        q.push({i,j});
+        dfs(grid, i+1, j, q);
+        dfs(grid, i-1, j, q);
+        dfs(grid, i, j+1, q);
+        dfs(grid, i, j-1, q);
     }
-    
-    int bfs(vector<vector<int>>& grid){ // finding the nearest bridge between visited island and unvisited island
-        
-        int d = 0; // dist
-        int mindist = INT_MAX; // to store minimum distance
-        
-        vector<vector<int>> dir = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-        
-        while (!q.empty()){
-            int n = q.size();
-            
-            while(n-- > 0){
-                auto a = q.front();
-                q.pop();
-
-                for (int h=0; h<4; h++) {
-                    int x = dir[h][0] + a.first;
-                    int y = dir[h][1] + a.second;
-                    
-                    if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 1) { // if the neighbor is 1, then check if its minimum distance
-                        mindist = min(mindist, d);
-                    }
-
-                    else if (x>=0 and x<grid.size() and y>=0 and y<grid[0].size() and grid[x][y] == 0) { // if the neighbor is 0, then mark it visited and add it to queue
-                        q.push({x, y});
-                        grid[x][y] = 2;
-                    }
-                }
-            }
-            d++; // increasing each level by distance + 1
-        }
-        
-        return mindist; // returning min dist found till end
-    }
-    
     int shortestBridge(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
         
-        bool flag = false;
+        queue<pair<int,int>>q;
         
-        for (int i=0; i<grid.size(); i++){
-            for (int j=0; j<grid[0].size(); j++){
-                if (grid[i][j] == 1 and !flag){ // found 1st '1' in the matrix
-                    dfs(grid, i, j); // dfs for marking the whole island visited
-                    q.push({i,j});
-                    flag = true;
+        bool found = false;
+        
+        //Identifying the island through DFS
+        for(int i=0;i<rows;i++){
+            if(found)
+                break;
+            for(int j=0;j<cols;j++){
+                if(grid[i][j] == 1){
+                    dfs(grid, i, j, q);
+                    found = true;
                     break;
                 }
             }
-            if (flag)
-                break;
         }
         
-        return bfs(grid); // bfs for getting min dist and returning it
+        //Performing Multisoucre BFS on the island and noting the 
+        //times to reach the target
+        
+        int steps = 0;
+        vector<pair<int, int>>directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        while(!q.empty()){
+            int size = q.size();
+            while(size--){
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                
+                for(auto dir:directions){
+                    int newX = x + dir.first;
+                    int newY = y + dir.second;
+                    
+                    if(newX<0 || newX>=rows || newY<0 || newY>=cols 
+                      || grid[newX][newY] == 2)
+                        continue;
+                    if(grid[newX][newY]==1)
+                        return steps;
+                    grid[newX][newY]=2;
+                    q.push({newX, newY});
+                }
+            }
+            steps++;
+        }
+        return -1;
     }
 };
