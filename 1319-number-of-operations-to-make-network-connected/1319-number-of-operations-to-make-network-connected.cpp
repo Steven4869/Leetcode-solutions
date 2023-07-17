6f0368dcc10870b7d0b58938cc9628e2fe1580ad
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-17 12:29:00 +0530

    Time: 117 ms (90.43%), Space: 37.9 MB (87.97%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,59 +1,59 @@
 class Solution {
 public:
-    //findParent function 
     int findParent(vector<int>&parent, int node){
-        if(node==parent[node]){
+        //If parent is its own node 
+        if(parent[node]==node)
             return node;
-        }
         return parent[node] = findParent(parent, parent[node]);
     }
-    //Union function 
-    void Union(vector<int> &parent, vector<int> &rank, int u, int v){
-        u = findParent(parent, u);
-        v = findParent(parent, v);
-        
-        if(rank[u]<rank[v]){
+    void unionSet(int u, int v, vector<int>&parent, vector<int>&rank){
+        if(rank[u]<rank[v])
             parent[u]=v;
-        }
-        else if(rank[u]>rank[v]){
+        else if(rank[u]>rank[v])
             parent[v]=u;
-        }
         else{
             parent[v]=u;
             rank[u]++;
         }
-        
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        //This is Union Find DFS Question 
-        
-        //Algorithm 
-        //1. Make the parent and rank array 
-        int N = connections.size();
-        
-        if(n-1 > N){
+        if(connections.size()<n-1)
             return -1;
-        }
+        
+        //Make the parent and rank arrays 
         vector<int>parent(n);
+        vector<int>rank(n, 0);
+        
+        //Make the parent of each node its own 
         for(int i=0;i<n;i++){
             parent[i]=i;
         }
         
-        vector<int>rank(n,0);
+        int extraConnections = 0;
         
-        //If the parent of 1 connection is not the other then union it 
-        for(int i=0;i<N;i++){
-            if(findParent(parent, connections[i][0])!=findParent(parent,connections[i][1])){
-                Union(parent, rank, connections[i][0], connections[i][1]);
-            }
+        for(auto &con:connections){
+            int u = con[0];
+            int v = con[1];
+            
+            int parentU = findParent(parent, u);
+            int parentV = findParent(parent, v);
+            
+            if(parentU != parentV)
+                unionSet(parentU, parentV, parent, rank);
+            else
+                extraConnections++;
         }
-        //If the element is its parent then increment the count 
-        int count=0;
+        
+        int numComponents = 0;
         for(int i=0;i<n;i++){
-            if(i==parent[i]){
-                count++;
+            if(parent[i]==i){
+                numComponents++;
             }
         }
-        return count-1;
+        
+        int numOperations = numComponents -1;
+        if(extraConnections < numOperations)
+            return -1;
+        return numOperations;
     }
 };
