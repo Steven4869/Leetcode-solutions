961e3f32e53ffc47a4ca2e1a0589ac03baf758d5
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-24 09:44:47 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (75.33%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
+    void generateSubset(vector<int>&nums, int start, vector<int>&current,
+                       vector<vector<int>>&result){
+        if(start == nums.size()){
+            result.push_back(current);
+            return;
+        }
+        current.push_back(nums[start]);
+        generateSubset(nums, start+1, current, result);
+        current.pop_back();
+        generateSubset(nums, start+1, current, result);
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        //It's a basic Backtracking question 
-        
-        //1. Intialise the result and current vector 
         vector<vector<int>>result;
         vector<int>current;
-        
-        //2. Call the backtrack function 
-        backtrack(nums, result, current, 0);
+        generateSubset(nums, 0, current, result);
         return result;
-    }
-    
-    void backtrack(vector<int>& nums, vector<vector<int>> &result, vector<int> &current, int                    source){
-        //Add the node to the result
-        result.push_back(current);
-        
-        //Traverse the neighbours elements 
-        for(int i=source;i<nums.size();i++){
-            //Insert it into current array 
-            current.push_back(nums[i]);
-            //Run the function 
-            backtrack(nums, result, current, i+1);
-            //Pop it from the array 
-            current.pop_back();
-        }
-        
     }
 };
