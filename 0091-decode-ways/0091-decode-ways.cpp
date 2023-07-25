1777205cbdcf77c7a68599c558acbc5e03ef36f3
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-25 16:35:03 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (43.42%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,47 +1,57 @@
 class Solution {
 public:
-    int numDecodings(string s) {
-        //Recursion 
-        //A character may be decoded alone or may be with pairing with other 
-        //Make a recursive function to call when the string is not empty 
+    int numDecodingRecursive(string &s, int index){
+        //If the index reaches to the string length 
+        if(index == s.length())
+            return 1;
+        //If the string's index is 0 then return 0
+        if(s[index] == '0')
+            return 0;
         
-        //Changes to make it to Memo dp 
-        int n = s.size();
-        vector<int>mem(n+1, -1);
-        mem[n]=1;
-        return s.empty()?0:decodeWays(0, s, mem);
+        //For the single digit 
+        int singleDigit = numDecodingRecursive(s, index+1);
         
-        // return s.empty()?0:decodeWays(0, s);
+        //For the double digit 
+        int doubleDigit = 0;
+        if(index+1<s.length() && (s[index] == '1' || (s[index] == '2' && 
+                                                    s[index+1] <= '6'))){
+            doubleDigit = numDecodingRecursive(s, index+2);
+        }
+        return singleDigit + doubleDigit;
     }
-//     int decodeWays(int pos, string &s){
-//         int n = s.size();
-//         //If position is the size then return 1;
-//         if(pos==n)
-//             return 1;
-//         //Sub string starting with 0 is not a valid string 
-//         if(s[pos]=='0')
-//             return 0;
-//         //Call it recursivley for the next string 
-//         int result = decodeWays(pos+1, s);
-//         //For the pair of two numbers 
-//         //if it starts with 1 then any number can come but if with 2 then till 6 it can come
-//         //as 26 is last number for the alphabet 
-//         if(pos<n-1 &&(s[pos] == '1' || (s[pos]=='2'&&s[pos+1]<'7')))
-//             result = result + decodeWays(pos+2, s);
-//         return result;
+    int numDecodingMemo(string &s, int index, vector<int>&dp){
+        if(index == s.length())
+            return 1;
+        if(s[index] == '0')
+            return 0;
         
-//         //TC : O(2^N)
-//         //To reduce TC we'll be using DP
-//         //Memoisation 
-//     }
-    
-    int decodeWays(int i, string &s, vector<int> &mem){
-        //Base condition 
-        if(mem[i]>-1) return mem[i];
-        if(s[i]=='0') return mem[i] = 0;
-        int res = decodeWays(i+1,s,mem);
-        if(i<s.size()-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) res+=decodeWays(i+2,s,mem);
-        return mem[i] = res;
+        if(dp[index] != -1)
+            return dp[index];
+        
+        //For the single digit 
+        int singleDigit = numDecodingMemo(s, index+1, dp);
+        
+        //For the double digit 
+        int doubleDigit = 0;
+        if(index+1<s.length() && (s[index] == '1' || (s[index] == '2' && 
+                                                    s[index+1] <= '6'))){
+            doubleDigit = numDecodingMemo(s, index+2, dp);
+        }
+        return dp[index] = singleDigit + doubleDigit;
+        
+    }
+    int numDecodings(string s) {
+        // //Base conditions 
+        // if(s.length() == 0 || s[0] == '0')
+        //     return 0;
+        // return numDecodingRecursive(s, 0);
+        
+        //Memo solution 
+        if(s.length() == 0 || s[0] == '0')
+            return 0;
+        int n = s.length();
+        vector<int>dp(n, -1);
+        return numDecodingMemo(s, 0, dp);
         
     }
 };
