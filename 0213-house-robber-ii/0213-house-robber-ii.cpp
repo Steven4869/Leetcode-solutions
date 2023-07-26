4a3badb7fd9567d8304d950b2b97ad3373331cd2
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-26 12:17:08 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.20%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -49,6 +49,37 @@ public:
         
         
     }
+    
+    int solveConstant(vector<int>&nums){
+        int n = nums.size();
+        if(n==0)
+            return 0;
+        if(n==1)
+            return nums[0];
+        if(n==2)
+            return max(nums[0], nums[1]);
+        
+        int prevPrev1 = nums[0];
+        int prev1 = max(nums[1], nums[0]);
+        int curr1 = prev1;
+        
+        for(int i=2;i<n-1;i++){
+            curr1 = max(prev1, prevPrev1 + nums[i]);
+            prevPrev1 = prev1;
+            prev1 = curr1;
+        }
+        int prevPrev2 = nums[1];
+        int prev2 = max(nums[1], nums[2]);
+        int curr2 = prev2;
+        
+        for(int i=3;i<n;i++){
+            curr2 = max(prev2, prevPrev2 + nums[i]);
+            prevPrev2 = prev2;
+            prev2 = curr2;
+        }
+        
+        return max(curr1, curr2);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n==1)
@@ -68,7 +99,11 @@ public:
         
         // Tabulation
         
-        return solveTabulation(nums);
+        // return solveTabulation(nums);
+        
+        //Constant Space 
+        
+        return solveConstant(nums);
         
         
     }
