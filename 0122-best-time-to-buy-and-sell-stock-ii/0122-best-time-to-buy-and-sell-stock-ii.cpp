855c6514abef1151c7bcf092a5ce59acf44c8a1c
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-26 17:03:39 +0530

    Time: 7 ms (75.75%), Space: 13.4 MB (36.84%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -39,14 +39,38 @@ public:
         
         return dp[buy][index] = profit;
     }
+    
+    int solveTabulation(vector<int> &prices){
+        int n = prices.size();
+        
+        vector<vector<int>>dp(2, vector<int>(n));
+        
+        //No stock is bought on Day 0 
+        dp[0][0] = 0;
+        
+        //Stock is bought on Day 0 so profit is -prices[0]
+        dp[1][0] = -prices[0];
+        
+        for(int i=1;i<prices.size();i++){
+            dp[1][i] = max(dp[1][i-1], -prices[i] + dp[0][i-1]);
+            
+            dp[0][i] = max(dp[0][i-1], prices[i] + dp[1][i-1]);
+        }
+        
+        return dp[0][n-1];
+    }
     int maxProfit(vector<int>& prices) {
         //Recursion 
         
         // return solve(prices, 1, 0);
         
         //Memoisation 
-        int n = prices.size();
-        vector<vector<int>>dp(2, vector<int>(n+1, -1));
-        return solveMemo(prices, 1, 0, dp);
+        // int n = prices.size();
+        // vector<vector<int>>dp(2, vector<int>(n+1, -1));
+        // return solveMemo(prices, 1, 0, dp);
+        
+        //Tabulation 
+        
+        return solveTabulation(prices);
     }
 };
