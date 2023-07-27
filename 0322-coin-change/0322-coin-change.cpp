e59d2fe36865a6452dba68c92d1c13c969b50354
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-27 10:50:25 +0530

    Time: 100 ms (75.35%), Space: 15.3 MB (55.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -33,6 +33,21 @@ public:
         }
         return dp[amount]=minCoins;
     }
+    
+    int solveTabulation(vector<int>&coins, int amount){
+        vector<int>dp(amount+1, INT_MAX);
+        dp[0]=0;
+        
+        for(int i=1;i<=amount;i++){
+            for(auto coin:coins){
+                if(coin<=i && dp[i-coin]!=INT_MAX)
+                    dp[i] = min(dp[i], dp[i-coin]+1);
+            }
+        }
+        
+        return dp[amount] == INT_MAX?-1:dp[amount];
+        
+    }
     int coinChange(vector<int>& coins, int amount) {
         
         //Recursion 
