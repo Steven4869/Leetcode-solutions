18c5e5b49c9a89f4c9374134f74fc6574fc09e70
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-07-31 16:59:31 +0530

    Time: 64 ms (99.89%), Space: 49 MB (98.34%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
+    
     bool isPalindrome(string s){
-        int left =0, right = s.length()-1;
-        while(left<right){
-            if(s[left]!=s[right]){
+        int i=0, j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j])
                 return false;
-            }
-            left++;
-            right--;
+            i++;
+            j--;
         }
         return true;
     }
-    void solve(string &s, int index, vector<vector<string>>&result,
-              vector<string>&current){
+    void solve(string &s, int index, vector<string>&current, vector<vector<string>>&result){
+        //Base case 
+        //if the index reaches the string's size
+        
         if(index == s.length()){
             result.push_back(current);
             return;
         }
         
+        //Check for the each substring 
         for(int i=index;i<s.length();i++){
             string substring = s.substr(index, i-index+1);
+            
+            //Check if the substring is a palindrome or not 
             if(isPalindrome(substring)){
+                //Push it into current
                 current.push_back(substring);
-                solve(s, i+1, result, current);
+                //Call for the next index
+                solve(s,i+1, current, result);
+                //Pop the current result once it backtracks 
                 current.pop_back();
             }
         }
+        
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>result;
         vector<string>current;
-        solve(s, 0, result, current);
+        solve(s,0,current,result);
         return result;
     }
 };
