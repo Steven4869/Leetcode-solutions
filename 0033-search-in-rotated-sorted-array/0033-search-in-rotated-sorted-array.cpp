6746992c4ce33a97bab8d0115b46b00dd30a2c78
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-08 10:38:15 +0530

    Time: 7 ms (28.91%), Space: 11 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
-    int getPivot(vector<int> &nums){
-        int n = nums.size();
-        int s =0, e = n-1;
-        while(s<e){
-            int mid = s + (e-s)/2;
-            if(nums[mid]>=nums[0]){
-                s=mid+1;
-            }
-            else{
-                e=mid;
-            }
+    int getPivot(vector<int>&nums){
+        int left = 0;
+        int right = nums.size()-1;
+        
+        while(left<right){
+            int mid = left + (right - left)/2;
+            if(nums[mid]>=nums[0])
+                left = mid+1;
+            else
+                right = mid;
         }
-        return s;
+        return left;
     }
-    int solve_recur(vector<int>& nums, int target, int start, int end){
-        if(start>end){
+    int binarySearch(vector<int>&nums, int start, int end, int target){
+        if(start>end)
             return -1;
-        }
         int mid = start + (end-start)/2;
-        if(nums[mid]==target){
+        
+        if(nums[mid] == target)
             return mid;
-        }
-        else if(nums[mid]>target){
-            return solve_recur(nums, target, start, mid-1);
-        }
-        else{
-            return solve_recur(nums, target, mid+1, end);
-        }
+        
+        else if(nums[mid]>target)
+            return binarySearch(nums, start, mid-1, target);
+        else
+            return binarySearch(nums, mid+1, end, target);
         
     }
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
+       int n = nums.size();
         int pivot = getPivot(nums);
-        int ans=-1;
         if((nums[pivot]<=target) && (target<=nums[n-1])){
-           return solve_recur(nums, target, pivot, n-1);
+           return binarySearch(nums, pivot, n-1, target);
         }
         else{
-            return solve_recur(nums, target, 0, pivot-1);
+            return binarySearch(nums, 0, pivot-1, target);
         }
+            
     }
 };
