6e16a5a3f67da7487fbd3dd71f4b4b051c789124
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-08 11:20:23 +0530

    Time: 6 ms (95.54%), Space: 10.4 MB (85.24%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -30,6 +30,25 @@ public:
         int notTake = 0 + solveMemo(nums, n, curr+1, prev, dp);
         return dp[curr][prev+1] = max(take, notTake);
     }
+    int solveBinary(vector<int>&nums, int n){
+        //Base Case 
+        if(n==0)
+            return 0;
+        
+        vector<int>result;
+        result.push_back(nums[0]);
+        
+        for(int i=1;i<n;i++){
+            if(nums[i]>result.back())
+                result.push_back(nums[i]);
+            else{
+                int index = lower_bound(result.begin(), result.end(), nums[i]) - result.begin();
+                result[index] = nums[i];
+            }
+        }
+        
+        return result.size();
+    }
     int lengthOfLIS(vector<int>& nums) {
         // We need to either include the current element or exclude it then based on it get the maximum of the calls and that's the length of LIS 
         
@@ -37,8 +56,12 @@ public:
         // return solve(nums, n, 0, -1);
         
         //Using Memoisation 
+        // int n = nums.size();
+        // vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        // return solveMemo(nums, n, 0, -1, dp);
+        
+        //Using Binary Search
         int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return solveMemo(nums, n, 0, -1, dp);
+        return solveBinary(nums, n);
     }
 };
