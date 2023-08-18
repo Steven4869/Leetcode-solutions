981647f6b8749a6c16241037c0744f08b800309e
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-18 12:20:04 +0530

    Time: 30 ms (83.22%), Space: 13.4 MB (42.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-    
-    void dfs(int i, int j, int rows, int cols, vector<vector<char>>& grid, vector<vector<int>> &visited){
-        if(i<0||j<0||i>=rows||j>=cols||visited[i][j]||grid[i][j]=='0')
-            return;
-        //Mark the visited 
-        visited[i][j]=1;
-        dfs(i-1,j,rows,cols,grid,visited);
-        dfs(i+1,j,rows,cols,grid,visited);
-        dfs(i,j-1,rows,cols,grid,visited);
-        dfs(i,j+1,rows,cols,grid,visited);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        //Using DFS 
-        //Approach 
-        //1. Get the rows and columns 
-        //2. make up a visited array 
-        //3. Run the dfs for all four directions 
+    void dfs(vector<vector<char>>& grid, int i, int j, int rows, int cols,                    vector<vector<int>>&visited){
+        //Base Case
         
+        if(i<0 || j<0 || i>=rows || j>=cols || visited[i][j] || grid[i][j]!='1')
+            return;
+        
+        visited[i][j] = 1;
+        dfs(grid, i+1, j, rows, cols, visited);
+        dfs(grid, i-1, j, rows, cols, visited);
+        dfs(grid, i, j+1, rows, cols, visited);
+        dfs(grid, i, j-1, rows, cols, visited);
+        
+}
+    int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        //Creating visited array 
-        vector<vector<int>>visited(rows,vector<int>(cols,0));
         
-        int count=0;
+        vector<vector<int>>visited(rows, vector<int>(cols, 0));
         
-        for(int it=0;it<rows;it++){
-            for(int jt=0;jt<cols;jt++){
-                if(!visited[it][jt]&&grid[it][jt]=='1'){
-                    //Call the recursvie function 
-                    dfs(it,jt,rows,cols,grid,visited);
-                    count++;
+        int countIsland = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    dfs(grid, i, j, rows, cols, visited);
+                    countIsland++;
                 }
             }
         }
-        return count;
+        return countIsland;
     }
 };
