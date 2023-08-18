2f06f8f367797490a1b1e1bc41b798d923836b07
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-18 11:59:22 +0530

    Time: 383 ms (95.80%), Space: 165 MB (77.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,45 +1,33 @@
 class LRUCache {
 public:
-    //Fixed size or the capacity
     int capacity;
-    //Making up the map for the access of the items
     unordered_map<int, pair<int, list<int>::iterator>>cache;
-    //Making the Doubly Linked List for keeping the track
-    list<int>lrulist;
+    list<int>lruList;
     LRUCache(int capacity) {
         this->capacity = capacity;
     }
     
     int get(int key) {
-        //If the element is not in the map then return -1
         if(cache.find(key) == cache.end())
             return -1;
-        //When the item is accessed it is moved to the head of DLL 
-        //to indicate the Most Recently Used operation
-        lrulist.splice(lrulist.begin(), lrulist, cache[key].second);
+        
+        lruList.splice(lruList.begin(), lruList, cache[key].second);
         return cache[key].first;
     }
     
     void put(int key, int value) {
-        //If the cache is not full then new item is added to DLL 
-        //into the hashmap
-        if(cache.find(key)!=cache.end()){
+        if(cache.find(key) != cache.end()){
             cache[key].first = value;
-            lrulist.splice(lrulist.begin(), lrulist, cache[key].second);
-            //Update the iterator
-            cache[key].second = lrulist.begin();
+            lruList.splice(lruList.begin(), lruList, cache[key].second);
             return;
         }
-        //If the cache is full 
         if(cache.size() == capacity){
-            //The last element in the list is removed
-            int evictKey = lrulist.back();
-            lrulist.pop_back();
-            //Erase it from the map
+            int evictKey = lruList.back();
+            lruList.pop_back();
             cache.erase(evictKey);
         }
-        lrulist.push_front(key);
-        cache[key]={value, lrulist.begin()};
+        lruList.push_front(key);
+        cache[key]={value, lruList.begin()};
     }
 };
 
