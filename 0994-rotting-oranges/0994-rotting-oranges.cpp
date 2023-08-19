ff3cd4a6bf49445d7b1691734de36ea52b77093c
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-19 22:59:08 +0530

    Time: 6 ms (66.96%), Space: 13.2 MB (51.45%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,37 +3,36 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int rows = grid.size();
         int cols = grid[0].size();
-        int freshOranges=0;
-        queue<pair<int,int>>q;
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j] == 1){
+        int freshOranges = 0;
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
                     freshOranges++;
-                }
-                else if(grid[i][j] == 2){
-                    q.push({i,j});
+                } else if (grid[i][j] == 2) {
+                    q.push({i, j});
                 }
             }
         }
-        
-        vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
         int time = 0;
-        
-        while(!q.empty() && freshOranges!=0){
+        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
+        while (!q.empty() && freshOranges != 0) {
             int size = q.size();
-            while(size--){
+            while (size--) {
                 int x = q.front().first;
                 int y = q.front().second;
 
                 q.pop();
 
-                for(auto dir:directions){
+                for (auto dir : directions) {
                     int newX = x + dir.first;
                     int newY = y + dir.second;
 
-                    if(newX>=0 && newY>=0 && newX<rows && newY<cols 
-                      && grid[newX][newY] == 1){
-                        grid[newX][newY]=2;
+                    if (newX >= 0 && newY >= 0 && newX < rows && newY < cols && grid[newX][newY] == 1) {
+                        grid[newX][newY] = 2;
                         q.push({newX, newY});
                         freshOranges--;
                     }
@@ -41,7 +40,7 @@ public:
             }
             time++;
         }
-        if(freshOranges != 0){
+        if (freshOranges != 0) {
             return -1;
         }
         return time;
