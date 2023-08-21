3a8976cd11bb3abb85a05ec90a4fd7fb3933270f
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-22 00:22:06 +0530

    Time: 3 ms (91.01%), Space: 13.8 MB (42.34%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>&cost, int n, vector<int>&dp){
+    int solve(vector<int>&cost, int n){
         //Base Case 
         if(n==0)
             return cost[0];
         if(n==1)
             return cost[1];
-        if(dp[n]!=-1){
+        
+        int ans;
+        return ans = min(solve(cost, n-1), solve(cost, n-2)) + cost[n];
+    }
+     int solveMemo(vector<int>&cost, int n, vector<int>&dp){
+        //Base Case 
+        if(n==0)
+            return cost[0];
+        if(n==1)
+            return cost[1];
+        if(dp[n]!=-1)
             return dp[n];
-        }
-        return dp[n] = cost[n]+min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        int ans = min(solveMemo(cost, n-1, dp), solveMemo(cost, n-2, dp)) + cost[n];
+         return dp[n] = ans;
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int n =cost.size();
+        int n = cost.size();
+        // int ans = min(solve(cost, n-1), solve(cost, n-2));
+        // return ans;
         vector<int>dp(n+1, -1);
-        int ans = min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        int ans = min(solveMemo(cost, n-1, dp), solveMemo(cost, n-2, dp));
         return ans;
     }
 };
