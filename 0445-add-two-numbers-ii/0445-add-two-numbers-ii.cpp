c461573d953aa7f676686f3ebbb06d3a56fe301f
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-25 12:32:35 +0530

    Time: 15 ms (99.70%), Space: 71.2 MB (54.57%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -10,50 +10,58 @@
  */
 class Solution {
 public:
-    ListNode *reverseList(ListNode *head){
-        ListNode *r=NULL;
-        ListNode *q=NULL;
-        ListNode *first=NULL;
-        while(head!=NULL){
-            r=q;
-            q=head;
-            head=head->next;
-            q->next=r;
-        }
-        first=q;
-        return q;
-    }
     
+    ListNode *reverseList(ListNode *head){
+        ListNode *nxt = NULL;
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        
+        while(curr!=NULL){
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        //Base Cases 
         if(l1==NULL && l2==NULL)
             return NULL;
         if(l1==NULL)
             return l2;
         if(l2==NULL)
             return l1;
-        ListNode *newHead = new ListNode(0);
-        ListNode *temp = newHead;
+        
+        ListNode *dummy = new ListNode(0);
+        ListNode *temp = dummy;
+        
+        //Reversing both the lists 
         l1 = reverseList(l1);
-        l2 =reverseList(l2);
-        int carry=0;
-        while(l1!=NULL || l2 !=NULL || carry){
-            int sum=0;
+        l2 = reverseList(l2);
+        
+        int carry = 0;
+        
+        while(l1!=NULL || l2!=NULL || carry!=0){
+            int sum = 0;
             if(l1!=NULL){
-                sum=sum+l1->val;
+                sum = sum + l1->val;
                 l1=l1->next;
             }
             if(l2!=NULL){
-                sum=sum+l2->val;
+                sum = sum + l2->val;
                 l2=l2->next;
             }
-            sum=sum+carry;
-            carry=sum/10;
-            ListNode *newNode = new ListNode(sum%10);
-            temp->next=newNode;
-            temp=temp->next;
+            
+            sum = sum + carry;
+            carry = sum/10;
+            
+            ListNode *node = new ListNode(sum%10);
+            temp->next = node;
+            temp = temp->next;
         }
-        newHead =reverseList(newHead->next);
-        return newHead;
+        dummy = reverseList(dummy->next);
+        return dummy;
         
     }
 };
