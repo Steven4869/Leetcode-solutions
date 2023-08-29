16e025de9445691e8a0b06d736301c71fcb0971c
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-29 12:11:59 +0530

    Time: 7 ms (26.38%), Space: 12.1 MB (62.97%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -7,18 +7,14 @@ public:
         while(i>=0 && nums[i]>=nums[i+1])
             i--;
         
-        //If we reach to the end 
-        if(i==-1){
-            reverse(nums.begin(), nums.end());
-            return;
+        if(i>=0){
+            int j = n-1;
+            while(j>=0 && nums[i]>=nums[j])
+                j--;
+            
+            swap(nums[i], nums[j]);
         }
-        int j = n-1;
-        while(i<j && nums[j]<=nums[i])
-            j--;
-        
-        swap(nums[i], nums[j]);
         
         reverse(nums.begin()+i+1, nums.end());
-        
     }
 };
