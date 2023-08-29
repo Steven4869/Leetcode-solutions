b615afc5b29cb542ae0acd2740046360ac4b84a4
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-29 11:45:06 +0530

    Time: 13 ms (15.32%), Space: 20.7 MB (29.46%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     string reverseWords(string s) {
-       string result="";
-        int i=0;
+        string result = "";
+        
+        int i = 0;
         int n = s.length();
+        
         while(i<n){
-            //Get to the point where word starts
-            while(i<n && s[i]==' '){
+            //getting to the point where the word starts 
+            while(i<n && s[i]==' ')
                 i++;
-            }
-            if(i>=n){
+            
+            if(i>=n)
                 break;
-            }
-            //Get to the end of the word until first space is encountered
+            
+            //Get the word 
             int j = i;
-            while(j<n&&s[j]!=' '){
+            while(j<n && s[j]!=' ')
                 j++;
-            }
-            //Get the substring 
+            
             string sub = s.substr(i, j-i);
             
-            //If the result length is 0 then just add the sub string to it otherwise include space as well
-            if(result.length()==0){
+            if(result.length() == 0)
                 result = sub;
-            }
-            else{
-                result = sub + " " + result;
-            }
+            else 
+                result = sub + ' ' + result;
             i=j+1;
-            
         }
         return result;
+        
     }
 };
