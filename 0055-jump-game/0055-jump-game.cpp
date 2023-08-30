fa79a3a68b18e445bff22b5205115e25c4e1f3a5
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-08-30 17:15:06 +0530

    Time: 1322 ms (7.20%), Space: 51.4 MB (6.73%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-//     void recur(vector<int>&nums, int i, bool &ans){
-//         if(ans == true){
-//             return;
-//         }
-//         if(i==nums.size()-1){
-//             ans = true;
-//             return;
-//         }
-        
-//         //recursive function 
-//         for(int j=1;j<=nums[i]&&j+i<nums.size();j++){
-//             recur(nums, i+j, ans);
-//         }
-//         //TC : O(N^N)
-//         //We can use DP to minimise the TC
-//     }
-    bool canJump(vector<int>& nums) {
-        //Brute Force Method 
-        //Get all the jump at once and check every jump 
-        //We'll follow recursion 
-        
-        // bool ans = false;
-        // recur(nums, 0, ans);
-        // return ans;
-        
-        //DP
-        vector<bool>dp(nums.size(), false);
-        dp[0]=true;
-        for(int i=0;i<nums.size();i++){
-            if(dp[i]==true){
-                for(int j=1;j<=nums[i]&&j+i<nums.size();j++){
-                    dp[i+j]=true;
-                    if(i+j==nums.size()-1)
-                        return true;
-                }
-            }
+    bool solve(vector<int>&nums, int idx, vector<int>&dp){
+        int n = nums.size();
+        if(idx == n-1)
+            return true;
+        if(idx>=n)
+            return false;
+        if(nums[idx] == 0)
+            return false;
+        if(dp[idx]!=-1)
+            return dp[idx];
+        for(int j=1;j<=nums[idx];j++){
+            if(solve(nums, idx+j, dp)){
+                dp[idx] = 1;
+                return true;
+            }            
         }
-        return dp[nums.size()-1];
+        dp[idx] = 0;
+        return false;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int>dp(nums.size()+1, -1);
+        return solve(nums, 0, dp);
     }
 };
