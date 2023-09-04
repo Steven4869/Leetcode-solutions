a0531665740b57440764d42a5a1a2b35c6d9cdb7
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-09-04 22:51:49 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (6.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int dp[101][101];
-    int recur(int m, int n, int i, int j){
-        if(i>=m||j>=n)
-            return 0;
-        if(i==m-1&&j==n-1)
+    int solve(int m, int n, int i, int j){
+        if(i==m-1 && j==n-1)
             return 1;
-        if(dp[i][j])
+        if(i>=m || j>=n)
+            return 0;
+        int ans = solve(m,n, i+1, j) + solve(m, n, i, j+1);
+        return ans;
+    }
+    int solveMemo(int m, int n, int i, int j, vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1)
+            return 1;
+        if(i>=m || j>=n)
+            return 0;
+        if(dp[i][j]!=-1)
             return dp[i][j];
-        return dp[i][j] = recur(m,n,i+1,j)+recur(m,n,i,j+1);
+        int ans = solveMemo(m,n, i+1, j, dp) + solveMemo(m, n, i, j+1, dp);
+        return dp[i][j] = ans;
     }
     int uniquePaths(int m, int n) {
-        return recur(m, n, 0, 0);
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+        // return solve(m,n, 0, 0, dp);
+        return solveMemo(m,n, 0, 0, dp);
     }
 };
