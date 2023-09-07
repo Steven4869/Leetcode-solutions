5b8abfc10088fd3470c17e8a4cc27de520736fd9
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-09-07 10:52:53 +0530

    Time: 141 ms (57.88%), Space: 65 MB (20.44%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,45 +1,50 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        //Base Case 
         if(n==1)
             return {0};
-        //Create the Adjacency List 
+        //Create the Adjacency List and store the dregee for each edge 
         unordered_map<int, list<int>>adjList;
         vector<int>degree(n, 0);
-        for(int i=0;i<edges.size();i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
+        
+        for(const auto&edge:edges){
+            int u = edge[0];
+            int v = edge[1];
             
             adjList[u].push_back(v);
             adjList[v].push_back(u);
             
             degree[u]++;
             degree[v]++;
+            
         }
-        //Making the queue and pushing all the nodes with degree 1 
+        
+        //Making up the queue and inserting only those whose degree is 1 
         queue<int>q;
+        vector<int>result;
         for(int i=0;i<n;i++){
             if(degree[i]==1)
                 q.push(i);
         }
-        vector<int>res;
+        //Perfrom the BFS for the degree 1 and keep on decrementing it for each degree in the adjacency 
         
         while(!q.empty()){
             int size = q.size();
-            res.clear();
+            result.clear();
             while(size--){
                 int frontNode = q.front();
                 q.pop();
-                res.push_back(frontNode);
-                for(auto i:adjList[frontNode]){
-                    degree[i]--;
-                    if(degree[i]==1)
-                        q.push(i);
+                result.push_back(frontNode);
+                
+                for(const auto& neighbor:adjList[frontNode]){
+                    degree[neighbor]--;
                     
+                    if(degree[neighbor]==1){
+                        q.push(neighbor);
+                    }
                 }
             }
         }
-        return res;
+        return result;
     }
 };
