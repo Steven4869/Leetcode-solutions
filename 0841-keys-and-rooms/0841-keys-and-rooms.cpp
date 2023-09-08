87b28bfc6ed45b8dc1e55b40943450e5a6e65b5c
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2023-09-08 16:52:17 +0530

    Time: 3 ms (98.31%), Space: 10.5 MB (32.87%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    
-    //DFS Function 
-    void dfs(vector<vector<int>>& rooms, vector<bool> &visited, int source){
-        //Mark it as visited 
-        visited[source]=true;
-        //Traverse the graph 
-        for(int i=0;i<rooms[source].size();i++){
-            //Check if it's visited or not 
-            if(!visited[rooms[source][i]]){
-                //Call the dfs function 
-                dfs(rooms, visited, rooms[source][i]);
+    void dfs(int node, vector<int>&visited, vector<vector<int>>& rooms){
+        visited[node]=1;
+        
+        for(const auto &neighbor:rooms[node]){
+            if(!visited[neighbor]){
+                dfs(neighbor, visited, rooms);
             }
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        //This is simple DFS Traversal 
-        //We'll have a visited array to check if we have visited or not 
-        //Algorithm 
-        //1. Make a visited array 
-        vector<bool>visited(rooms.size(),false);
-        //2. Call the dfs function 
-        dfs(rooms, visited, 0);//0 refers to source node 
-        //3. Traverse the visited array if everything is visited then mark true 
-        for(int i=0;i<rooms.size();i++){
+        int n = rooms.size();
+        vector<int>visited(n, 0);
+        
+        dfs(0, visited, rooms);
+        for(int i=0;i<n;i++){
             if(!visited[i]){
                 return false;
             }
